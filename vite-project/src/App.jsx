67e549746cad0ca4d8acc5fe295e517e8b5bf880
karla lautero <karla.lautero@estudiantesunibague.edu.co>
2026-05-05 18:00:40 -0500

    ultimos cambios

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,18 +5,34 @@ import {io} from 'socket.io-client'
 
 function App() {
   const [socket, setSocket] = useState()
+  const [inputMessage, setInputMessage] = useState()
 
   useEffect(() => {
     const newSocket = io("localhost:3000")
+    setSocket(newSocket)
+
+    return () => {
+      newSocket.disconnect()
+    }
   }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (socket) {
+      socket.emit("message", inputMessage)
+    }
+
+  }
 
 
 
   return (
     <div>
       <form>
-        <input type ="texts" placeholder="Escribe el mensaje"/>
-        <button>Enviar</button>
+        <input type ="texts" placeholder="Escribe el mensaje"
+         onChange={(e) => setInputMessage(e.target.value)}
+         />
+        <button type="submit">Enviar</button>
       </form>
     </div>
   )
